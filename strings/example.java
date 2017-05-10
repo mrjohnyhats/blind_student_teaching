e18f8232d642a60616471864ff60560b8ea53f92
mrjohnyhats <jonbutterminecraftian@gmail.com>
2017-05-10 10:55:40 -0700

    added strings

---
+++ strings/example.java
@@ -0,0 +1,10 @@
+ // # begin example
+ // declaration of a string variable
+ String myString;
+ // definition of the string variable
+ myString = "hello, I am a string!";
+ // declaration and definition of a string
+ String aString = "I am also a string";
+ // addition (concatenation) of 2 strings
+ "what I am: " + myString
+ // # end example
---
+++ strings/exercise.java
@@ -0,0 +1,9 @@
+// # begin exercise
+// at the marker, set 2 string variables. Then set a third string variable named i to the concatenation of your 2 other strings. It should print
+class Main {
+    public static void main(String[] args) {
+        // # start below this marker:
+
+        System.out.println(i);
+    }
+}
